d13f4b8c9b0e3271e9ac6ca92dca16b9a895a82e
abey546 <131441171+abey546@users.noreply.github.com>
2024-04-26 16:17:15 +0000

    updated

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 
-auth = Blueprint("auth", "__name__")
+auth = Blueprint("auth", __name__)
 
 @auth.route("/signup")
 def signup():
